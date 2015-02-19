7696805b9adb1b7f9263f1ed747c7ce8b1e91f74
gpsingh12 <gurpreet_1441@yahoo.com>
2015-02-19 04:54:23 +0000

    operators

--- task_03.py
+++ task_03.py
@@ -6,3 +6,8 @@ EXPENSE = 14.23
 LOOKS_NICE = True
 MAX_EXPENSE = 12
 GET_OUT_ALIVE = False
+
+if not (LOOKS_NICE is True and EXPENSE <= MAX_EXPENSE):
+    GET_OUT_ALIVE = True
+    SACRIFICE = '{}'.format(GET_OUT_ALIVE)
+    print SACRIFICE
