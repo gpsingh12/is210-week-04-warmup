c8f86cde65883b914867a1a7f4f281ac7b4d5175
gpsingh12 <gurpreet_1441@yahoo.com>
2015-02-20 01:34:15 +0000

    BP-status

--- task_05.py
+++ task_05.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
-""" Docstring."""
-BP = raw_input('What is your blood pressure? ')
-BP_STATUS = 'Low'
+# -*- coding: utf-8 -*-
+""" Docstring. """
 
-if   BP <= 89:
+BP = int(raw_input('What is your blood pressure? '))
 
+if BP <= 89:
     BP_STATUS = 'Low'
-    print 'Your status is currently {}'.format(BP_STATUS)
-
 elif 90 <= BP <= 119:
-     BP_STATUS = 'Ideal'
-     print 'Your status is currently {}'.format(BP_STATUS)
-
+    BP_STATUS = 'Ideal'
 elif 120 <= BP <= 139:
-     BP_STATUS = 'Warning'
-     print 'Your status is currently {}'.format(BP_STATUS)
-
+    BP_STATUS = 'Warning'
 elif 140 <= BP <= 159:
     BP_STATUS = 'High'
-    print 'Your status is currently {}'.format(BP_STATUS)
-
 else:
     BP_STATUS = 'Emergency'
-    print  'Your status is currently {}'.format(BP_STATUS)
+print  'Your status is currently:{}'.format(BP_STATUS)
+ 
+
